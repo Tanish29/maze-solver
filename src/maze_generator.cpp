026ac4eba93e7d548500a96a2d3bb41b1a89e1b9
Tanish <bhatt.t2002@gmail.com>
2026-03-20 22:08:43 +1300

    Add stack include and rename RDFS constructor

--- src/maze_generator.cpp
+++ src/maze_generator.cpp
@@ -1,7 +1,9 @@
 #include "maze_generator.hpp"
 
+// MazeGenerator
 MazeGenerator::MazeGenerator(std::string algo_name) : name(algo_name) {}
 
-RDFSGenerator::RDFSGenerator(std::string algo_name) : MazeGenerator(algo_name) {}
+// RDFSGenerator
+RDFSIterativeGenerator::RDFSIterativeGenerator(std::string algo_name) : MazeGenerator(algo_name) {}
 
 void RDFSGenerator::generate(Maze& maze) {}
